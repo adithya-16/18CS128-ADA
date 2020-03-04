8f3ad39a14f0d76805b8543fa1cbab82e84f5c37
adithya-16 <53932174+adithya-16@users.noreply.github.com>
2020-03-04 12:54:36 +0530

    Program 5

--- Week4.py
+++ Week4.py
@@ -11,24 +11,28 @@ l = [ 5, 3, 9 ,15, 2, 1, 40, 39 ]
 insertion_sort(l)
 print(l)
 
-def meet_sort(arr):
+def insertion_sort(arr):
     for i in range(1,len(arr)):
         item = arr[i]
         j = i - 1
-        while j >= 0 and (arr[j][1] - arr[j][0]) > (item[1] - item[0]):
-            arr[j+1] = arr[j]
-            j -= 1
-        arr[j+1] = item
-    for i in range(1,len(arr)):
-        item = arr[i]
-        j = i - 1
-        while j >= 0 and arr[j][0] >= item[1]:
+        while j >= 0 and arr[j] > item:
             arr[j+1] = arr[j]
             j -= 1
         arr[j+1] = item
 
+l = [ 5, 3, 9 ,15, 2, 1, 40, 39 ]
+insertion_sort(l)
+print(l)
+
+def bbl(a):
+    n = len(a)
+    for i in range(n-1):
+        for j in range(n-i-1):
+            if (a[j][1] - a[j][0]) > (a[j+1][1] - a[j+1][0]) or a[j][0] >= a[j+1][1]:
+                a[j], a[j+1] = a[j+1], a[j]
+
 def count_meetings(m):
-    meet_sort(m)
+    bbl(m)
     print(m)
     count = 1
     print(f"Meeting 1: {m[0][0]}-{m[0][1]}")
@@ -41,5 +45,6 @@ def count_meetings(m):
 m = [ [10,12], [10,11], [11, 12], [10,11], [21,22], [20,21], [12, 13], [19, 22], [8,9] ]
 count_meetings(m)
 
+        
         
             
